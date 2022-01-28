cafedc006c9c02fbe3b665a04ef061d22d524b3d
AndrianiClaudio <Claudioandriani1994@gmail.com>
2022-01-28 15:52:58 +0100

    Stampa paragrafo e sua lunghezza

--- index.php
+++ index.php
@@ -5,6 +5,10 @@
 // Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare.
 // ========================================
 // Definizione variabili
+// paragrafo di testo
+$paragraph = "Questo paragrafo ha una lunghezza di 50 caratteri!";
+// variabile contentente lunghezza del paragrafo
+$parLength = strlen($paragraph);
 ?>
 
 <!DOCTYPE html>
@@ -13,12 +17,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- rimuove errore GET favicon.ico -->
+    <link rel="shortcut icon" href="#">
     <title>PHP - BADWORDS</title>
 </head>
 <body>
     <p>
         <?php
-        // stampa paragrafo
+        // var_dump($paragraph); check variabile
+        // stampa paragrafo e sua lunghezza
+        echo 'Contenuto del paragrafo: ', $paragraph,
+        '<br>Lunghezza del paragrafo: ', $parLength, ' caratteri';
         ?>
     </p>
 </body>
